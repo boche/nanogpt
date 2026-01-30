c93cdc6dae6f8e1baf7789efcb253e80751569d3
bochet <aimboy@outlook.com>
2026-01-29 18:40:52 -0800

    Add todo to further improve the model

--- v2.py
+++ v2.py
@@ -26,16 +26,19 @@ def build_vocab(text):
     return chars, encode, decode
 
 class MultiHeadAttention(torch.nn.Module):
+    # add local parameters: block_size
     def __init__(self, num_head, emb_size, head_size):
         super().__init__()
         mask = torch.zeros(block_size, block_size).masked_fill(
             torch.tril(torch.ones(block_size, block_size)) == 0, float('-inf')
         )
+        # TODO: merge q, k, v linear
         self.q_head = nn.Linear(emb_size, emb_size)
         self.k_head = nn.Linear(emb_size, emb_size)
         self.v_head = nn.Linear(emb_size, emb_size)
         self.register_buffer('mask', mask)
         self.proj = torch.nn.Linear(num_head * head_size, emb_size)
+        # Question: can dropout with same ratio be merged? does it affect gradient when merging
         self.attn_dropout = nn.Dropout(dropout_ratio)
         self.proj_dropout = nn.Dropout(dropout_ratio)
     
@@ -44,23 +47,32 @@ class MultiHeadAttention(torch.nn.Module):
         q = self.q_head(x).view(B, S, num_head, C // num_head).transpose(1, 2) # B, N, S, H
         k = self.k_head(x).view(B, S, num_head, C // num_head).transpose(1, 2) # B, N, S, H
         v = self.v_head(x).view(B, S, num_head, C // num_head).transpose(1, 2) # B, N, S, H
+        # TODO: add positional encoding
 
+        # TODO: verify the mask is working as expected
+        # TODO: flash attention
+        # TODO: Don't store self attention because it's expensive
         self.attention = F.softmax(q @ k.transpose(-2,-1) / math.sqrt(C // num_head) + self.mask, dim=-1) # B, N, S, S
         dot_product = self.attn_dropout(self.attention) @ v # B, N, S, H
         dot_product = dot_product.transpose(-2,-3).reshape(B, S, -1) # B, S, N * H
+        # Question: add a non-linear layer between attn and final projection
         return self.proj_dropout(self.proj(dot_product))
 
 
 class DecoderBlock(torch.nn.Module):
     def __init__(self):
         super().__init__()
+        # TODO: Visualize attention weight
         self.MHA = MultiHeadAttention(num_head, emb_size, emb_size // num_head)
         self.proj = nn.Sequential(
             nn.Linear(emb_size, 4 * emb_size),
+            # TODO: try GELU
             nn.ReLU(),
             nn.Linear(4 * emb_size, emb_size),
             nn.Dropout(dropout_ratio)
         )
+        # Question: how to compute gradient for layer/batch norm?
+        # TODO: implement layer norm directly
         self.ln1 = nn.LayerNorm(emb_size)
         self.ln2 = nn.LayerNorm(emb_size)
         
@@ -78,12 +90,10 @@ class NanoGPT(nn.Module):
         self.decoder_block = nn.ModuleList(
             DecoderBlock() for i in range(decoder_layer)
         )
-        # self.decoder = DecoderBlock()
         self.lm_head = nn.Linear(emb_size, vocab_size)
 
     def forward(self, input, target):
         state = self.token_embedding_table(input) # BATCH x SEQ_LEN x EMB_SIZE
-        # state = self.decoder(state)
         for i in range(decoder_layer):
             state = self.decoder_block[i](state)
         logits = self.lm_head(state) # BATCH x SEQ_LEN x VOCAB_SIZE
@@ -92,13 +102,15 @@ class NanoGPT(nn.Module):
             return logits, None
         else:
             B, S, C = logits.shape
+            # TODO: implement cross entropy directly
             loss = F.cross_entropy(logits.view(B*S, C), target.view(-1))
             return logits, loss
 
     @torch.no_grad()
     def generate(self, idx, max_new_tokens):
         for i in range(max_new_tokens):
-            logits, _ = self(idx[:, -block_size:], None)
+            # TODO: handle case when idx has fewer than block_size input
+            logits, _ = self(idx[:, -block_size:], None) # logits: B, S, V
             logits = logits[:, -1, :] # BATCH x VOCAB_SIZE
             probs = F.softmax(logits, dim=-1) # BATCH x VOCAB_SIZE
             token = torch.multinomial(probs, num_samples=1)
@@ -110,6 +122,7 @@ def get_batch(text, split):
     data = train if split == 'train' else val
 
     ix = torch.randint(len(data) - block_size - 1, (batch_size,))
+    # TODO: implement this more natively in matrix
     xb = torch.stack([data[i:i+block_size] for i in ix])
     yb = torch.stack([data[i+1:i+1+block_size] for i in ix]) # b * seq_len
 
@@ -120,13 +133,20 @@ with urllib.request.urlopen(url) as response:
 
 chars, encode, decode = build_vocab(text)
 vocab_size = len(chars)
+# TODO: don't store unnecessary data on gpu
 text_indices = torch.tensor(encode(text), dtype=torch.long).to(device) 
 torch.manual_seed(seed)
 
 model=NanoGPT(vocab_size).to(device)
 
+# TODO: print model parameters size
+# TODO: use gradient clipping to stablize model, how to simulate gradient instability
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
+# TODO: add monitoring for running time
+# TODO: add monitoring for maxmimal gpu memory used
+# TODO: try low precision representation
+# TODO: try torch.compile
 cum_loss = 0
 for i in range(10000):
     xb, yb = get_batch(text_indices, 'train')
@@ -138,6 +158,7 @@ for i in range(10000):
     optimizer.step()
     if i % 100 == 0:
         model.eval()
+        # TODO: add more samples to eval loss
         with torch.no_grad():
             xb, yb = get_batch(text_indices, 'test')
             _, loss = model(xb, yb)
