6f5558844a16ca07158e6b63be5c6a226b584e89
bochet <aimboy@outlook.com>
2026-02-15 10:50:23 -0800

    add a file for model definition

--- v2.py
+++ v2.py
@@ -1,9 +1,7 @@
 import urllib.request
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import math
 import time
+from NanoGPT import NanoGPT, loss_fn
 
 
 # hyperparameters
@@ -40,108 +38,6 @@ def build_vocab(text):
     decode = lambda s: "".join(i2ch[i] for i in s)
     return chars, encode, decode
 
-class MultiHeadAttention(torch.nn.Module):
-    # add local parameters: block_size
-    def __init__(self, num_head, emb_size, head_size):
-        super().__init__()
-        if use_gqa:
-            self.qkv_head = nn.Linear(emb_size, emb_size + emb_size * 2 // group_size)
-        else:
-            self.qkv_head = nn.Linear(emb_size, emb_size * 3)
-
-        self.proj = torch.nn.Linear(num_head * head_size, emb_size)
-        # Question: can dropout with same ratio be merged? does it affect gradient when merging
-        self.attn_dropout = nn.Dropout(dropout_ratio)
-        self.proj_dropout = nn.Dropout(dropout_ratio)
-        if not use_flash_attn:
-            self.register_buffer('bias', torch.tril(torch.ones(block_size, block_size).view(1, 1, block_size, block_size)))
-    
-    def forward(self, x):
-        B, S, C = x.shape
-        if use_gqa:
-            qkv_prod = self.qkv_head(x) # B, S, Mixed
-            q = qkv_prod[:, :, :emb_size].view(B, S, num_head, -1).permute(0, 2, 1, 3) # B, H, S, C
-            k = qkv_prod[:, :, emb_size:emb_size+emb_size//group_size].view(B, S, num_head//group_size, -1).permute(0, 2, 1, 3) # B, H/G, S, C
-            v = qkv_prod[:, :, emb_size+emb_size//group_size:].view(B, S, num_head//group_size, -1).permute(0, 2, 1, 3) # B, H/G, S, C
-
-        else:
-            qkv = self.qkv_head(x).view(B, S, 3, num_head, C // num_head).permute(2, 0, 3, 1, 4) # 3, B, N, S, H
-            q, k, v = qkv[0], qkv[1], qkv[2] # B, N, S, H
-
-        if use_flash_attn:
-            # TODO: implement flash attention with Triton
-            if use_gqa:
-                dot_product = F.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=dropout_ratio if self.training else 0, is_causal=True, enable_gqa=use_gqa)
-            else:
-                dot_product = F.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=dropout_ratio if self.training else 0, is_causal=True)
-
-        else:
-            # TODO: implement other attention, e.g. MQA, GQA, MLA, delta attention
-            qk_product = q @ k.transpose(-2,-1) / math.sqrt(C // num_head)
-            self.attention = F.softmax(
-                qk_product.masked_fill(self.bias[:, :, :S, :S] == 0, float('-inf')),
-                dim = -1
-            ) # B, N, S, S
-            dot_product = self.attn_dropout(self.attention) @ v # B, N, S, H
-
-        dot_product = dot_product.transpose(-2,-3).reshape(B, S, -1) # B, S, N * H -> B, S, C
-        # Question: add a non-linear layer between attn and final projection
-        return self.proj_dropout(self.proj(dot_product))
-
-class DecoderBlock(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        # TODO: Visualize attention weight
-        self.MHA = MultiHeadAttention(num_head, emb_size, emb_size // num_head)
-        self.proj = nn.Sequential(
-            nn.Linear(emb_size, 4 * emb_size),
-            nn.GELU(),
-            nn.Linear(4 * emb_size, emb_size),
-            nn.Dropout(dropout_ratio)
-        )
-        # Question: how to compute gradient for layer/batch norm?
-        # TODO: implement layer norm directly
-        # TODO: compare layer norm with RMS norm, why pick RMS norm?
-        self.alpha = nn.Parameter(torch.ones(2,1))
-        self.beta = nn.Parameter(torch.ones(2,1))
-
-    def forward(self, x):
-        # X: B, S, C
-        x = self.alpha[0] * x + self.beta[0] * self.MHA(F.rms_norm(x, (x.size(-1),)))
-        # return self.alpha[1] * x + self.beta[1] * self.proj(self.ln2(x))
-        return x + self.proj(F.rms_norm(x, (x.size(-1),)))
-    
-class NanoGPT(nn.Module):
-    def __init__(self, vocab_size):
-        super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, emb_size)
-        # TODO: implement RoPE, sinusoidal
-        self.position_embedding_table = nn.Embedding(block_size, emb_size)
-        # TODO: implement hybrid attention with sssl switch 
-        self.decoder_block = nn.ModuleList(
-            DecoderBlock() for i in range(decoder_layer)
-        )
-        self.lm_head = nn.Linear(emb_size, vocab_size)
-
-    def forward(self, input):
-        B, S = input.shape
-        state = self.token_embedding_table(input) # B, S, C
-        pos_emb = self.position_embedding_table(torch.arange(S, device=device)).unsqueeze(0) # 1, S, E
-        state = state + pos_emb
-        for i in range(decoder_layer):
-            state = self.decoder_block[i](state)
-        return self.lm_head(F.rms_norm(state, (state.size(-1),))) # BATCH x SEQ_LEN x VOCAB_SIZE
-
-    @torch.no_grad()
-    def generate(self, idx, max_new_tokens):
-        for i in range(max_new_tokens):
-            logits = self(idx[:, -block_size:]) # logits: B, S, V
-            logits = logits[:, -1, :] # BATCH x VOCAB_SIZE
-            probs = F.softmax(logits, dim=-1) # BATCH x VOCAB_SIZE
-            token = torch.multinomial(probs, num_samples=1)
-            idx = torch.cat((idx, token), dim=1) # append to sequence
-        return idx
-
 def get_batch(text, split):
     train, val = text[:int(0.9*len(text))], text[int(0.9*len(text)):]
     data = train if split == 'train' else val
@@ -153,11 +49,6 @@ def get_batch(text, split):
 
     return xb, yb
 
-def loss_fn(logits, target):
-    B, S, C = logits.shape
-    # TODO: implement cross entropy directly
-    return F.cross_entropy(logits.view(B*S, C), target.view(-1))
-
 with urllib.request.urlopen(url) as response:
     text = response.read().decode("utf-8")
 
@@ -166,7 +57,7 @@ vocab_size = len(chars)
 text_indices = torch.tensor(encode(text), dtype=torch.long)
 torch.manual_seed(seed)
 
-model = NanoGPT(vocab_size).to(device)
+model = NanoGPT(vocab_size, emb_size, block_size, decoder_layer, num_head, dropout_ratio, use_flash_attn, group_size).to(device)
 torch.set_float32_matmul_precision('high')
 model = torch.compile(model)
 
